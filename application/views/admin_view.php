a3beb05c483ba3765630c11489ef947b2db3647e
kerusiperak <ameeradnan@gmail.com>
2016-01-13 11:09:46 +0800

    admin view updated/menu disable action features/temp

--- application/views/admin_view.php
+++ application/views/admin_view.php
@@ -12,7 +12,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <i class="fa fa-cog"></i>
         </a>
       </div>      
-        
+         <ul class="nav navbar-nav hidden-xs">
+        <li>
+            <span style="font-size:30px;padding-right: 10px; padding-top: 5px;color: #B4B9BC;">Attendance Management</span>
+        </li>
+        <li>
+          <a onclick="return true;" href="http://www.celcom1cbc.com/dashboard" target="_blank" style="border-left:1px dashed #CECECE;color: #8B8B8B;letter-spacing: 1px"><span class="btn btn-success">Go to Pi1M <span class="not-connected-text"></span></span></a>
+        </li>
+      </ul>
       <ul class="nav navbar-nav navbar-right m-n hidden-xs nav-user user">
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">
