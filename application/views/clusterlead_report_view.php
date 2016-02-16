9c75808e796a5aef918cefdbbdd4a214e3d58ed5
kerusiperak <ameeradnan@gmail.com>
2016-02-16 23:13:07 +0800

    - change tag

--- application/views/clusterlead_report_view.php
+++ application/views/clusterlead_report_view.php
@@ -6,7 +6,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 //alert(get_post['cluster']+ 'aaaaaa');
 $(document).ready(function() {
-<? $query = http_build_query($_POST); ?>
+<?php $query = http_build_query($_POST); ?>
 
 $('#dateFrom').datepicker({ dateFormat: 'dd-mm-yy' });
     $('#dateTo').datepicker({ dateFormat: 'dd-mm-yy' });      
