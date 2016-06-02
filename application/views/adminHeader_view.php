ddad330d0acad7435a0850db7ebadae6e49d0884
kerusiperak <ameeradnan@gmail.com>
2016-06-02 11:01:29 +0800

    -commit jquery/https ourbound url

--- application/views/adminHeader_view.php
+++ application/views/adminHeader_view.php
@@ -44,7 +44,7 @@ defined ('BASEPATH') or exit('No direct access allowed!');
     <script src="<?php echo base_url();?>js/ie/respond.min.js"></script>
     <script src="<?php echo base_url();?>js/ie/excanvas.js"></script>
   <![endif]-->
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>-->
   <!--<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>-->
   <!--<script src="<?php echo base_url();?>js/jquery.min.js"></script>-->
